2f3b904ffd670d1dc65714559014f78d91f7c4b6
Hasitha24 <hasithak002@gmail.com>
2024-10-22 21:55:30 +0530

    Find the Wolf Game in JS

---
+++ script.js
@@ -0,0 +1,36 @@
+// const inputArray = ["sheep","wolf","sheep","sheep"];
+
+
+// let currentAnimal ="";
+// for (i=0; i<= ((inputArray.length)-1); i++)
+// {
+//     currentAnimal += inputArray[i] + " ";
+// } 
+// document.getElementById("demo").innerHTML = inputArray.indexOf("wolf");
+
+// if ((inputArray.indexOf("wolf")) == (inputArray.length-1)){
+// document.getElementById("demo").innerHTML = "Pls go away and stop eating my sheep";
+// }
+// else{
+//     document.getElementById("demo").innerHTML = "Oi! Sheep number "+ (inputArray.indexOf("wolf")+1)+ " ! You are about to be eaten by a wolf!";
+// }
+
+// The above as a function
+
+// function checkWolfPosition(inputArray){
+//     let wolfIndex = inputArray.indexOf("wolf");
+
+//     if (wolfIndex == inputArray.length-1){
+//         return "Pls go away and stop eating my sheep";
+//     }
+//     else {
+//         return "Oi! Sheep number "+ (inputArray.indexOf("wolf")+1)+ " ! You are about to be eaten by a wolf!";
+//     }
+// }
+// const animals = ["sheep","wolf","sheep","sheep"];
+// let message = checkWolfPosition(animals);
+
+// //document.getElementById("demo").innerHTML = message;
+// console.log(message);
+
+  
